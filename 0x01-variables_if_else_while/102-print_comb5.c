4aeb4f4eb293bf3cb65e3589f62de95499081192
aahmedehab <ahmedehabahmed2004@gmail.com>
2023-07-14 04:35:09 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i, j, k, l;
+
+for (i = 0; i < 100; i++)
+{
+for (j = 0; j < 100; j++)
+{
+if (i < j)
+{
+k = i / 10;
+l = i % 10;
+putchar(k + '0');
+putchar(l + '0');
+putchar(' ');
+k = j / 10;
+l = j % 10;
+putchar(k + '0');
+putchar(l + '0');
+if (i != 98 || j != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+
+return (0);
+}
