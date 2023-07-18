1be69f0993dce7c7597e946d8cd6cf683ce77575
aahmedehab <ahmedehabahmed2004@gmail.com>
2023-07-18 10:57:04 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isalpha - checks for alphabetic character 
+ * _isalpha - checks for alphabetic character
  * @c: the character to be checked
  * Return: 1 for alphabetic character or 0 for anything else
  */
